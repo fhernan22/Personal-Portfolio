1c10d1743b8bc3e6dcf3f7f91b7fe69e775c446b
fhernan22 <fhern103@fiu.edu>
2021-06-14 22:34:11 -0400

    Finished hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hi there! <br />
+          I'm Fidel Hernandez
+        </SectionTitle>
+        <SectionText>
+          I am a creative developer who enjoys building full stack applications,
+          video games, neural networks, and anything that brings value to the
+          world!
+        </SectionText>
+        <Button
+          onClick={() => (window.location.href = "mailto:fhern103@fiu.edu")}
+        >
+          Contact Me
+        </Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
