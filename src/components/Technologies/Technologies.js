3b354da78415b309083915d831e4076471fa9b1b
fhernan22 <fhern103@fiu.edu>
2021-06-15 22:29:07 -0400

    Finished technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,80 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact } from "react-icons/di";
+import { SiUnrealengine, SiTensorflow } from "react-icons/si";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies. From Full-Stack web development
+      and game development to data science libraries
+    </SectionText>
+    <List style={{ gridTemplateColumns: "repeat(4,1fr)" }}>
+      <ListItem>
+        <picture>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+            React.js and tools <br /> like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <SiTensorflow size="2.5rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Machine Learning</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Python and Tensorflow
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <SiUnrealengine size="2.5rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Game Dev</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Unreal Engine 4
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
 );
 
 export default Technologies;
